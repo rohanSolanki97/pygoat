4fc941003710262bf65329341be888803da588a5
rohanSolanki97 <rohan.solanki@hcltech.com>
2026-03-23 16:13:21 +0530

    Add delta unit test

--- test/test_views.py
+++ test/test_views.py
@@ -16,15 +16,12 @@ def _make_request(method="POST", authenticated=True, post=None):
 
 
 def test_ssrf_lab_rejects_non_allowlisted_blog_and_does_not_open_file(mocker):
-    # Arrange
     request = _make_request(method="POST", post={"blog": "../../etc/passwd"})
     open_mock = mocker.patch("builtins.open", autospec=True)
     render_mock = mocker.patch.object(views, "render", autospec=True)
 
-    # Act
     views.ssrf_lab(request)
 
-    # Assert: should not attempt to open arbitrary path
     open_mock.assert_not_called()
     render_mock.assert_called()
     assert render_mock.call_args[0][1] == "Lab/ssrf/ssrf_lab.html"
@@ -32,19 +29,18 @@ def test_ssrf_lab_rejects_non_allowlisted_blog_and_does_not_open_file(mocker):
 
 
 def test_ssrf_lab_allows_allowlisted_blog_and_reads_via_safe_join(mocker):
-    # Arrange
     request = _make_request(method="POST", post={"blog": "safe_blog.txt"})
     mocker.patch.object(views.os.path, "dirname", return_value="/app/introduction")
-    join_mock = mocker.patch.object(views.os.path, "join", return_value="/app/introduction/safe_blog.txt")
+    join_mock = mocker.patch.object(
+        views.os.path, "join", return_value="/app/introduction/safe_blog.txt"
+    )
 
     m = mocker.mock_open(read_data="SAFE CONTENT")
     open_mock = mocker.patch("builtins.open", m, create=True)
     render_mock = mocker.patch.object(views, "render", autospec=True)
 
-    # Act
     views.ssrf_lab(request)
 
-    # Assert
     join_mock.assert_called_once_with("/app/introduction", "safe_blog.txt")
     open_mock.assert_called_once_with("/app/introduction/safe_blog.txt", "r")
     render_mock.assert_called()
