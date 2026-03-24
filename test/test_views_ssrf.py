b9aeddb4e2b4c14b1e6d97d4743935d27c7f7dd4
rohanSolanki97 <rohan.solanki@hcltech.com>
2026-03-24 11:03:25 +0530

    Add delta unit test

--- test/test_views_ssrf.py
+++ test/test_views_ssrf.py
@@ -3,34 +3,36 @@ import os
 import pytest
 from django.test import RequestFactory
 
-from introduction.views import ssrf_lab
+from introduction import views
 
 
 @pytest.mark.django_db
-class TestSSRFLab:
+class TestSsrfLabFileAccess:
     def setup_method(self):
         self.factory = RequestFactory()
 
-    def test_ssrf_lab_rejects_unauthorized_file(self):
-        request = self.factory.post("/ssrf/lab", data={"blog": "../../etc/passwd"})
-        request.user = type("User", (), {"is_authenticated": True})()
+    def test_ssrf_lab_allows_only_whitelisted_files(self, mocker, settings):
+        request = self.factory.post("/ssrf/lab", data={"blog": "safe_blog.txt"})
+        request.user = mocker.Mock(is_authenticated=True)
 
-        response = ssrf_lab(request)
+        dirname = os.path.dirname(views.__file__)
+        safe_path = os.path.join(dirname, "safe_blog.txt")
+        mock_open = mocker.mock_open(read_data="SAFE CONTENT")
+        mocker.patch("builtins.open", mock_open)
+
+        response = views.ssrf_lab(request)
+
+        assert response.status_code == 200
+        assert b"SAFE CONTENT" in response.content
+
+    def test_ssrf_lab_rejects_non_whitelisted_files(self, mocker):
+        request = self.factory.post("/ssrf/lab", data={"blog": "../../../etc/passwd"})
+        request.user = mocker.Mock(is_authenticated=True)
+
+        mock_open = mocker.patch("builtins.open")
+
+        response = views.ssrf_lab(request)
 
         assert response.status_code == 200
         assert b"No blog found" in response.content
-
-    def test_ssrf_lab_allows_only_whitelisted_files(self, mocker, tmp_path, settings):
-        dirname = os.path.dirname(__file__)
-        safe_file = tmp_path / "safe_blog.txt"
-        safe_file.write_text("Safe content")
-
-        mocker.patch("introduction.views.os.path.dirname", return_value=str(tmp_path))
-
-        request = self.factory.post("/ssrf/lab", data={"blog": "safe_blog.txt"})
-        request.user = type("User", (), {"is_authenticated": True})()
-
-        response = ssrf_lab(request)
-
-        assert response.status_code == 200
-        assert b"Safe content" in response.content
+        mock_open.assert_not_called()
